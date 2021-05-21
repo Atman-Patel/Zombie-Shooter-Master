ad4d1165cead13e2538e7122071bfe2eb709c3d3
Atman <73461380+Atman-Patel@users.noreply.github.com>
2021-05-21 10:48:23 +0530

    Delete zombie-shooter-master directory

--- zombie-shooter-master/Final_tabbed_component_with_js/tabbed component/script.js
+++
@@ -1,9 +0,0 @@
-let list = [1,2,3,4,5]
-list.forEach((num)=>{
-    let tab = document.querySelector(`#tab-${num}`)
-    document.addEventListener('keyup',(e)=>{
-        if(e.key == num){
-            tab.checked = true
-        }
-    })
-})
